055e37131b3aeefce8f8afc5ebedbbccc94b4f6f
Pedro P. Candel <kusorbox@gmail.com>
2010-05-12 13:41:52 -0700

    Successful FreeBSD-8.0 build.

--- ext/zetta/extconf.rb
+++ ext/zetta/extconf.rb
@@ -3,6 +3,26 @@ require 'mkmf'
 pkg_name = File.basename(File.dirname(File.expand_path(__FILE__)))
 
 dir_config(pkg_name)
+
+if PLATFORM =~ /freebsd/
+  $CFLAGS+= " -I/usr/src/cddl/contrib/opensolaris/lib/libzfs/common"
+  $CFLAGS+= " -I/usr/src/cddl/contrib/opensolaris/lib/libzpool/common"
+  $CFLAGS+= " -I/usr/src/cddl/compat/opensolaris/include"
+  $CFLAGS+= " -I/usr/src/cddl/compat/opensolaris/lib/libumem"
+  $CFLAGS+= " -I/usr/src/sys/cddl/compat/opensolaris"
+  $CFLAGS+= " -I/usr/src/cddl/contrib/opensolaris/head"
+  $CFLAGS+= " -I/usr/src/cddl/contrib/opensolaris/lib/libuutil/common"
+  $CFLAGS+= " -I/usr/src/cddl/contrib/opensolaris/lib/libzfs/common"
+  $CFLAGS+= " -I/usr/src/cddl/contrib/opensolaris/lib/libumem/common"
+  $CFLAGS+= " -I/usr/src/cddl/contrib/opensolaris/lib/libnvpair"
+  $CFLAGS+= " -I/usr/src/sys/cddl/contrib/opensolaris/uts/common"
+  $CFLAGS+= " -I/usr/src/sys/cddl/contrib/opensolaris/uts/common/fs/zfs"
+  $CFLAGS+= " -I/usr/src/sys/cddl/contrib/opensolaris/uts/common/sys"
+  $CFLAGS+= " -DNEED_SOLARIS_BOOLEAN"
+
+  $LDFLAGS+= " -lavl -lgeom -lbsdxml -lsbuf -lnvpair -luutil -lutil"
+end
+
 have_library("c", "main")
 
 # Check for prerequisite ZFS header and library.
